ab9253455059e8074742741df0ee85ab65378b50
Ketan Shukla <ketankshukla@gmail.com>
2024-08-27 20:27:35 -0700

    added prettier module throgh npm, created .prettierrc

--- newapp/README.md
+++
@@ -1,8 +0,0 @@
-# django-startapp-template
-
-A slightly modified template for django's "django-admin startapp" command. Adds in urls.py and a directory for tests.
-
-## Usage
-```
-$ ./manage.py startapp --template=https://github.com/cfc603/django-startapp-template/archive/master.zip myapp
-```
--- newapp/urls.py
+++ newapp/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import path
 
 from newapp import views
 
@@ -6,4 +6,6 @@ from newapp import views
 urlpatterns = [
     # url(r'^$', views.index_view, name='index'),
     # url(r'^another/$', views.another_view, name='another'),
+    path("", views.home)
+
 ]
