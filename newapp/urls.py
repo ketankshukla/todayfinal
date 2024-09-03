d5689ee9c39f5a3a5f5a38100fb7143cd894de63
Ketan Shukla <ketankshukla@gmail.com>
2024-09-02 23:08:47 -0700

    removed duplicate entry

--- newapp/urls.py
+++ newapp/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from newapp import views
 
-
 urlpatterns = [
     # url(r'^$', views.index_view, name='index'),
     # url(r'^another/$', views.another_view, name='another'),
