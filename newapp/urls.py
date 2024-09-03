791886f685d398839c9c6ba8e16a68f3a4cc69d6
Ketan Shukla <ketankshukla@gmail.com>
2024-09-02 23:29:20 -0700

    updated requirements and changed webpage title

--- newapp/urls.py
+++ newapp/urls.py
@@ -5,6 +5,5 @@ from newapp import views
 urlpatterns = [
     # url(r'^$', views.index_view, name='index'),
     # url(r'^another/$', views.another_view, name='another'),
-    path("", views.home)
-
+    path("", views.home),
 ]
